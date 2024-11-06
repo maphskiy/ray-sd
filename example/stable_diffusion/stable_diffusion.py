fe7d4c7b7d0e89cf9964edff6d746d88b54e7133
maphskiy <maphskiy@gmail.com>
2024-11-06 15:57:07 +0300

    sd

--- example/stable_diffusion/stable_diffusion.py
+++ example/stable_diffusion/stable_diffusion.py
@@ -8,7 +8,7 @@ from ray import serve
 
 app = FastAPI()
 
-@serve.deployment(route_prefix="/")
+@serve.deployment()
 @serve.ingress(app)
 class APIIngress:
     def __init__(self, diffusion_model_handle) -> None:
@@ -28,16 +28,7 @@ class APIIngress:
         return Response(content=file_stream.getvalue(), media_type="image/png")
 
 
-@serve.deployment(
-    ray_actor_options={"num_gpus": 1},
-    autoscaling_config={
-        "min_replicas": int(os.environ.get("MIN_REPLICAS", 1)),
-        "max_replicas": int(os.environ.get("MAX_REPLICAS", 1)),
-        "target_num_ongoing_requests_per_replica": 1,
-        "upscale_delay_s": 10,
-        "downscale_delay_s": 600,
-    },
-)
+@serve.deployment()
 class StableDiffusionV2:
     def __init__(self):
         from diffusers import EulerDiscreteScheduler, StableDiffusionPipeline

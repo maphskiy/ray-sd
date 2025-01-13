15256ca0e8c4b8ec3a5a9c0a1a25e8a195b0166f
maphskiy <maphskiy@gmail.com>
2025-01-13 18:35:13 +0300

    2 model

--- example/stable_diffusion/stable_diffusion.py
+++ example/stable_diffusion/stable_diffusion.py
@@ -50,7 +50,7 @@ class StableDiffusionV2:
     def __init__(self):
         from diffusers import EulerDiscreteScheduler, StableDiffusionPipeline
 
-        model_id = "stabilityai/stable-diffusion-x1"
+        model_id = "stabilityai/stable-diffusion-2"
 
         scheduler = EulerDiscreteScheduler.from_pretrained(
             model_id, subfolder="scheduler"
